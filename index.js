538caf1582e8b845e6e36ce7d0f584c16e1eb606
rakeshrauniyar12 <rakeshrauniyar12>
2023-04-02 20:16:19 +0530

    changed in project section

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-
 let sidemenu = document.getElementById("sidemenu");
 
-function openmenu(){
-    sidemenu.style.right = "0";
+function openmenu() {
+  sidemenu.style.right = "0";
 }
-function closemenu(){
-    sidemenu.style.right = "-220px";
+function closemenu() {
+  sidemenu.style.right = "-220px";
 }
 let openResume = document.getElementById("openResume");
-openResume.onclick=()=>{
-    window.open("./fw19_0048-Abhinandan-Rauniyar-Resume (2).pdf", '_blank');
-}
+openResume.onclick = () => {
+  window.open("./fw19_0048-Abhinandan-Rauniyar-Resume (2).pdf", "_blank");
+};
+document.querySelector("#live").addEventListener("click", () => {
+  window.location.href =
+    "https://drive.google.com/file/d/1ZrjVTMSBIURhAiG4Rn5PwUQLUf135Blx/view?usp=sharing";
+});
+document.querySelector("#code").addEventListener("click", () => {
+  window.location.href = "https://github.com/Rajul-Tiwari/majestic-vest-8576";
+});
